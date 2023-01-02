cbe5d9c872510e04bcf35cfdb60d9fff2c9d1b4d
anshul45 <80778090+anshul45@users.noreply.github.com>
2023-01-02 08:43:48 +0530

    created tmdb api's

--- server/index.js
+++ server/index.js
@@ -12,3 +12,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 const port = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Mongodb connected");
+    server.listen(port, () => {
+      console.log(`Server is Listning on ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log({ err });
+    process.exit(1);
+  });
