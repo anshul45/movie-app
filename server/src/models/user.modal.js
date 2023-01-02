bd7691277c9ed7f24a5a53dbbf61a4a00a96d538
anshul45 <80778090+anshul45@users.noreply.github.com>
2023-01-02 09:14:05 +0530

    model created

---
+++ server/src/models/user.modal.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import modelOptions from "./model.option.js";
+import crypto from "crypto";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    displayName: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    salt: {
+      type: String,
+      require: true,
+    },
+  },
+  modelOptions
+);
+
+userSchema.method.setPassword = function (password) {
+  this.salt = crypto.randomBytes(16).toString("hex");
+  this.password = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, "sha512")
+    .toString("hex");
+};
+
+userSchema.method.validPassword = function (password) {
+  const hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, "sha512")
+    .toString("hex");
+  return this.password === hash;
+};
+
+const userModel = mongoose.model("User", userSchema);
+
+export default userModel;
