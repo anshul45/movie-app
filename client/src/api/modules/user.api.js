9e009897cf832e1f8139adcd540141b67ee018e1
anshul45 <80778090+anshul45@users.noreply.github.com>
2023-01-10 11:03:45 +0530

    user api

---
+++ client/src/api/modules/user.api.js
@@ -0,0 +1,58 @@
+import privateClient from "../client/private.client";
+import publicClient from "../client/public.client";
+
+const userEndpoints = {
+  signin: "user/signin",
+  signup: "user/signup",
+  getInfo: "user/info",
+  passwordUpdate: "user/update-password",
+};
+
+const userApi = {
+  signin: async ({ username, password }) => {
+    try {
+      const response = await publicClient.post(userEndpoints.signin, {
+        username,
+        password,
+      });
+      return { response };
+    } catch (err) {
+      return { err };
+    }
+  },
+  signup: async ({ username, password, confirmPassword, displayName }) => {
+    try {
+      const response = await publicClient.post(userEndpoints.signup, {
+        username,
+        password,
+        confirmPassword,
+        displayName,
+      });
+      return { response };
+    } catch (err) {
+      return { err };
+    }
+  },
+  getInfo: async ({}) => {
+    try {
+      const response = await privateClient.get(userEndpoints.getInfo);
+      return { response };
+    } catch (err) {
+      return { err };
+    }
+  },
+  passwordUpdate: async ({ password, newPassword, confirmNewPassword }) => {
+    try {
+      const response = await privateClient.put(userEndpoints.signup, {
+        password,
+        newPassword,
+        confirmNewPassword,
+      });
+      return { response };
+    } catch (err) {
+      return { err };
+    }
+  },
+};
+
+export default userApi;
