88b3ef2fbfa567e41a9d4acc396aa5edaf701637
anshul45 <80778090+anshul45@users.noreply.github.com>
2023-01-10 10:23:16 +0530

    created client api

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -8,3 +8,25 @@ const privateClient = axios.create({
     encode: (params) => queryString.stringify(params),
   },
 });
+
+privateClient.interceptors.request.use(async (config) => {
+  return {
+    ...config,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("actkn")}`,
+    },
+  };
+});
+
+privateClient.interceptors.response.use(
+  (response) => {
+    if (response && response.data) return response.data;
+    return response;
+  },
+  (err) => {
+    throw err.response.data;
+  }
+);
+
+export default privateClient;
