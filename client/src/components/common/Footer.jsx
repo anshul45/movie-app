620a7c1bc6a44719ec9af3392d2305a60984a36b
anshul45 <80778090+anshul45@users.noreply.github.com>
2023-01-15 08:35:51 +0530

    fix bugs

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -1,5 +1,6 @@
-import { Box, Button, Container, Paper, Stack } from "@mui/material";
+import { Box, Button, Paper, Stack } from "@mui/material";
 import React from "react";
+import Container from "./Container";
 import Logo from "./Logo";
 import menuConfigs from "../../configs/menu.configs";
 import { Link } from "react-router-dom";
@@ -17,7 +18,12 @@ const Footer = () => {
           <Logo />
           <Box>
             {menuConfigs.main.map((item, index) => (
-              <Button key={index} sx={{ color: "inherit" }} component={Link}>
+              <Button
+                key={index}
+                sx={{ color: "inherit" }}
+                component={Link}
+                to={item.path}
+              >
                 {item.display}
               </Button>
             ))}
