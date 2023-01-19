6ca24b4f32b77f19c77b1f09908538c18f38c636
anshul45 <80778090+anshul45@users.noreply.github.com>
2023-01-19 05:42:35 +0530

    fix bugs

--- server/src/models/user.modal.js
+++ server/src/models/user.modal.js
@@ -27,14 +27,14 @@ const userSchema = new mongoose.Schema(
   modelOptions
 );
 
-userSchema.method.setPassword = function (password) {
+userSchema.methods.setPassword = function (password) {
   this.salt = crypto.randomBytes(16).toString("hex");
   this.password = crypto
     .pbkdf2Sync(password, this.salt, 1000, 64, "sha512")
     .toString("hex");
 };
 
-userSchema.method.validPassword = function (password) {
+userSchema.methods.validPassword = function (password) {
   const hash = crypto
     .pbkdf2Sync(password, this.salt, 1000, 64, "sha512")
     .toString("hex");
@@ -44,5 +44,3 @@ userSchema.method.validPassword = function (password) {
 const userModel = mongoose.model("User", userSchema);
 
 export default userModel;
-
-select: false;
