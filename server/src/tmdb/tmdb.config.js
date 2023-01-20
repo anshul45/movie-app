63ca2573af4b0ed918962e5fc848d5a23e1ba8d9
anshul45 <80778090+anshul45@users.noreply.github.com>
2023-01-20 12:48:18 +0530

    1 error remaining

--- server/src/tmdb/tmdb.config.js
+++ server/src/tmdb/tmdb.config.js
@@ -3,7 +3,7 @@ const key = process.env.TMDB_KEY;
 
 const getUrl = (endpoint, params) => {
   const qs = new URLSearchParams(params);
-  return `${baseUrl}${endpoint}?api_key=${key}&${qs}`;
+  return `${baseUrl}${endpoint}/?api_key=${key}&${qs}`;
 };
 
 export default { getUrl };
