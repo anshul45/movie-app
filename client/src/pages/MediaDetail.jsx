12b06aad0bf07b0920444479038f74c580f3cf6e
anshul45 <80778090+anshul45@users.noreply.github.com>
2023-01-23 11:13:20 +0530

    movie backdrop

--- client/src/pages/MediaDetail.jsx
+++ client/src/pages/MediaDetail.jsx
@@ -24,6 +24,7 @@ import { addFavorite, removeFavorite } from "../redux/features/userSlice";
 
 import CastSlide from "../components/common/CastSlide";
 import MediaVideosSlide from "../components/common/MediaVideosSlide";
+import BackdropSlide from "../components/common/BackdropSlide";
 
 const MediaDetail = () => {
   const { mediaType, mediaId } = useParams();
@@ -262,6 +263,19 @@ const MediaDetail = () => {
           </Container>
         </div>
         {/* media videos */}
+
+        {/* media backdrop */}
+
+        {media.images.backdrops.length > 0 && (
+          <Container header="backdrops">
+            <BackdropSlide backdrops={media.images.backdrops} />
+          </Container>
+        )}
+        {/* media backdrop */}
+
+        {/* media posters */}
+        {media.images}
+        {/* media posters */}
       </Box>
     </>
   ) : null;
