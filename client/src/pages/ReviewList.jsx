c0b0bab51ba19e98336c6bfbafbeeba3d8583c02
anshul45 <80778090+anshul45@users.noreply.github.com>
2023-02-05 22:54:04 +0530

    created review

--- client/src/pages/ReviewList.jsx
+++ client/src/pages/ReviewList.jsx
@@ -1,4 +1,19 @@
-import React from "react";
+import { LoadingButton } from "@mui/lab";
+import { Box, Button, Divider, Stack, Typography } from "@mui/material";
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
+import tmdbConfigs from "../api/configs/tmdb.configs";
+import reviewApi from "../api/modules/review.api";
+import Container from "../components/common/Container";
+import uiConfigs from "../configs/ui.configs";
+import { setGlobalLoading } from "../redux/features/globalLoadingSlice";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { routesGen } from "../routes/routes";
+
+const ReviewItem = ({ review, onRemoved }) => {};
 
 const ReviewList = () => {
   return <div>ReviewList</div>;
